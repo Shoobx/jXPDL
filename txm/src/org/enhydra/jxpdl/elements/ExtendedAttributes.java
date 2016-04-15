e78bc42ce5ef71ba6d5752a14e8772b097c6e3e7
Sasa Bojanic <sasaboy@together.at>
2016-04-15 11:51:11 +0000

    # Caching ExtendedAttributes' entries by name to improve performance when calling getFirstExtendedAttributeForName() method
    git-svn-id: svn://svn.code.sf.net/p/jxpdl/code/trunk@303 b102af9a-decc-4e9f-b8e4-f589e172fd83

--- txm/src/org/enhydra/jxpdl/elements/ExtendedAttributes.java
+++ txm/src/org/enhydra/jxpdl/elements/ExtendedAttributes.java
@@ -20,6 +20,7 @@ package org.enhydra.jxpdl.elements;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.enhydra.jxpdl.XMLCollection;
 import org.enhydra.jxpdl.XMLComplexElement;
@@ -36,10 +37,8 @@ import org.enhydra.jxpdl.utilities.SequencedHashMap;
 public class ExtendedAttributes extends XMLCollection {
 
    /**
-    * Map that holds the information about all the ExtendedAttribute elements within
-    * collection with a different names (keys represent the names, and values the list of
-    * ExtendedAttribute elements). This map can be used only if this element's mode is
-    * read-only and when caches are initialized.
+    * Map that holds the information about all the ExtendedAttribute elements within collection with a different names (keys represent the names, and values the
+    * list of ExtendedAttribute elements). This map can be used only if this element's mode is read-only and when caches are initialized.
     */
    public transient SequencedHashMap eaMap;
 
@@ -47,6 +46,8 @@ public class ExtendedAttributes extends XMLCollection {
     * String representing an XML part defining this collection.
     */
    protected String extAttribsString;
+
+   protected boolean internalCachesInitialized = false;
 
    /** Constructs a new object with the given {@link XMLComplexElement} as a parent. */
    public ExtendedAttributes(XMLComplexElement parent) {
@@ -56,19 +57,18 @@ public class ExtendedAttributes extends XMLCollection {
    public void makeAs(XMLElement el) {
       super.makeAs(el);
       this.extAttribsString = ((ExtendedAttributes) el).extAttribsString;
+      this.internalCachesInitialized = false;
    }
 
    /**
-    * Generates new {@link ExtendedAttribute} object. This object is not member of the collection
-    * yet, it has to be explicitly added to the collection.
+    * Generates new {@link ExtendedAttribute} object. This object is not member of the collection yet, it has to be explicitly added to the collection.
     */
    public XMLElement generateNewElement() {
       return new ExtendedAttribute(this);
    }
 
    /**
-    * Returns the first {@link ExtendedAttribute} from this collection that has the given name, or
-    * null if there is no one.
+    * Returns the first {@link ExtendedAttribute} from this collection that has the given name, or null if there is no one.
     */
    public ExtendedAttribute getFirstExtendedAttributeForName(String name) {
       ExtendedAttribute ea = null;
@@ -84,6 +84,18 @@ public class ExtendedAttributes extends XMLCollection {
     */
    public void initCaches(XMLInterface xmli) {
       super.initCaches(xmli);
+      initInternalCaches();
+      if (parent instanceof Application) {
+         getExtendedAttributesString();
+      }
+   }
+
+   protected void initInternalCaches() {
+      if (eaMap != null) {
+         eaMap.clear();
+      } else {
+         eaMap = new SequencedHashMap();
+      }
       Iterator it = elements.iterator();
       while (it.hasNext()) {
          ExtendedAttribute ea = (ExtendedAttribute) it.next();
@@ -95,9 +107,7 @@ public class ExtendedAttributes extends XMLCollection {
          l.add(ea);
          eaMap.put(eaName, l);
       }
-      if (parent instanceof Application) {
-         getExtendedAttributesString();
-      }
+      internalCachesInitialized = true;
    }
 
    /** Initializes string representing XML part of this element. */
@@ -122,8 +132,7 @@ public class ExtendedAttributes extends XMLCollection {
    }
 
    /**
-    * Returns the string representation of XML part of this element. If this element's
-    * mode is not read-only, RuntimeException is thrown.
+    * Returns the string representation of XML part of this element. If this element's mode is not read-only, RuntimeException is thrown.
     */
    public String getExtendedAttributesString() {
       if (!isReadOnly) {
@@ -151,18 +160,18 @@ public class ExtendedAttributes extends XMLCollection {
     * Returns true if there is at least one ExtendedAttribute with given name.
     */
    public boolean containsElement(String name) {
-      if (isReadOnly && cachesInitialized) {
-         return eaMap.containsKey(name);
+      if (!internalCachesInitialized) {
+         initInternalCaches();
       }
-
-      Iterator it = elements.iterator();
-      while (it.hasNext()) {
-         ExtendedAttribute ea = (ExtendedAttribute) it.next();
-         if (ea.getName().equals(name)) {
-            return true;
+      boolean ret = eaMap.containsKey(name);
+      if (ret) {
+         ArrayList al = (ArrayList) eaMap.get(name);
+         if (!checkList(al, name)) {
+            initInternalCaches();
+            ret = eaMap.containsKey(name);
          }
       }
-      return false;
+      return ret;
    }
 
    /**
@@ -183,24 +192,93 @@ public class ExtendedAttributes extends XMLCollection {
     * Returns a list of all ExtendedAttribute elements with specified name.
     */
    public ArrayList getElementsForName(String name) {
-      if (isReadOnly && cachesInitialized) {
-         return (ArrayList) eaMap.get(name);
+      if (!internalCachesInitialized) {
+         initInternalCaches();
       }
+      ArrayList al = (ArrayList) eaMap.get(name);
+      if (!checkList(al, name)) {
+         initInternalCaches();
+         al = (ArrayList) eaMap.get(name);
+      }
+      return al;
+   }
 
-      ArrayList l = new ArrayList();
-      Iterator it = elements.iterator();
-      while (it.hasNext()) {
-         ExtendedAttribute ea = (ExtendedAttribute) it.next();
-         if (ea.getName().equals(name)) {
-            l.add(ea);
+   protected boolean checkList(ArrayList al, String name) {
+      if (al != null) {
+         for (Object object : al) {
+            if (!((ExtendedAttribute) object).getName().equals(name)) {
+               return false;
+            }
          }
       }
-      return l;
+      return true;
+   }
+
+   public void add(XMLElement el) {
+      super.add(el);
+      if (internalCachesInitialized) {
+         ExtendedAttribute ea = (ExtendedAttribute) el;
+         ArrayList al = getElementsForName(ea.getName());
+         if (al != null) {
+            al.add(ea);
+         } else {
+            al = new ArrayList();
+            al.add(ea);
+            eaMap.put(ea.getName(), al);
+         }
+      }
+   }
+
+   public boolean add(int no, XMLElement el) {
+      boolean ret = super.add(no, el);
+      if (internalCachesInitialized) {
+         ExtendedAttribute ea = (ExtendedAttribute) el;
+         ArrayList al = getElementsForName(ea.getName());
+         if (al != null) {
+            al.add(ea);
+         } else {
+            al = new ArrayList();
+            al.add(ea);
+            eaMap.put(ea.getName(), al);
+         }
+      }
+      return ret;
+   }
+
+   public boolean addAll(List els) {
+      boolean ret = super.addAll(els);
+      internalCachesInitialized = false;
+      return ret;
+   }
+
+   protected XMLElement removeElement(int no) {
+      XMLElement ret = super.removeElement(no);
+      if (internalCachesInitialized) {
+         ExtendedAttribute ea = (ExtendedAttribute) ret;
+         ArrayList al = getElementsForName(ea.getName());
+         if (al != null) {
+            al.remove(ea);
+         }
+      }
+      return ret;
+   }
+
+   public boolean removeAll(List els) {
+      boolean ret = super.removeAll(els);
+      internalCachesInitialized = false;
+      return ret;
+   }
+
+   public boolean reposition(XMLElement el, int newPos) {
+      boolean ret = super.reposition(el, newPos);
+      internalCachesInitialized = false;
+      return ret;
    }
 
    public Object clone() {
       ExtendedAttributes d = (ExtendedAttributes) super.clone();
       d.extAttribsString = this.extAttribsString;
+      d.internalCachesInitialized = false;
 
       return d;
    }
